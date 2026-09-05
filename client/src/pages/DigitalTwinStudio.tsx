281793cc6495f903307825a8ffd2e80b062f11e1
HUSSAIN <65435678+hussain-alayfei@users.noreply.github.com>
2026-09-05 12:56:11 +0300

    Load digital twin responsive hardening after studio styles

--- client/src/pages/DigitalTwinStudio.tsx
+++ client/src/pages/DigitalTwinStudio.tsx
@@ -1,4 +1,5 @@
 import { EngineeringTwinStudioV6 } from "@/components/EngineeringTwinStudioV6";
+import "../engineering-twin-v6-hardening.css";
 
 export function DigitalTwinStudioPage() {
   return <EngineeringTwinStudioV6 />;
